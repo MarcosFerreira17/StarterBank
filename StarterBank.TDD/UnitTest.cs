6b6737d1922d1f5dd94bc6203971ab5043c478e3
mnia <marcos.ferreira@gft.com>
2022-05-02 18:40:52 -0300

    adicionando projeto

---
+++ StarterBank.TDD/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StarterBank.TDD
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ StarterBank/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using StarterBank.Model;
+
+namespace SearchBank.Data
+{
+    public class SearchBankContext : DbContext
+    {
+        public DbSet<CaixaEletronico> CaixaEletronico { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+    }
+}
